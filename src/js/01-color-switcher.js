a8f42cdfbd4aefbda48a11e15b6280ec1b141028
Kosano <bogdan.kosyak@ukr.net>
2021-09-10 12:19:44 +0300

    ended 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
-
+const buttonStart = document.querySelector("[data-start]");
+const buttonStop = document.querySelector("[data-stop]");
+const body = document.querySelector("body");
+let timer = null;
+buttonStop.disabled = true;
+buttonStart.addEventListener("click", () => {
+  timer = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000)
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+})
+buttonStop.addEventListener("click", () => {
+  clearInterval(timer);
+  buttonStop.disabled = true;
+  buttonStart.disabled = false;
+    
+})
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
